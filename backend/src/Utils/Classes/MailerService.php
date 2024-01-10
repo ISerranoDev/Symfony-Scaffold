b11de0c4fe637ff2124a7518c8ba7d63be6b0fb8
Ismael Serrano <ismael.serrano@serglobin.es>
2024-01-10 10:25:34 +0100

    Changes for prevent Google to mark email as spam

--- backend/src/Utils/Classes/MailerService.php
+++ backend/src/Utils/Classes/MailerService.php
@@ -4,21 +4,24 @@ namespace App\Utils\Classes;
 
 use Exception;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 
 class MailerService
 {
     private MailerInterface $mailer;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, ParameterBagInterface $parameterBag)
     {
         $this->mailer = $mailer;
+        $this->parameterBag = $parameterBag;
     }
 
     public function sendEmail(string $to, string $subject, string $template, array $context = [], array $attachments = []): bool
     {
-        $email = (new TemplatedEmail())->from('iserrano.developer@gmail.com')
+        $email = (new TemplatedEmail())->from($this->parameterBag->get('mailer-email'))
             ->to($to)
             ->subject('Soporte - ' . $subject)
             ->htmlTemplate($template)
